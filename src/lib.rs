2be1e7eb4a021702f256c35c4071b0f54120a09e
Kyle Schreiber <kyle@80x24.net>
2024-04-27 22:44:11 -0700

    Add key encrypt and decrypt

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,7 @@ pub fn chapoly_decrypt_ietf(
     ciphertext: &[u8],
     aad: &[u8],
 ) -> Result<Vec<u8>, JsValue> {
-    Ok(
-        kestrel_crypto::chapoly_decrypt_ietf(key, nonce, ciphertext, aad)
-            .map_err(|e| fmt_error(&e))?,
-    )
+    kestrel_crypto::chapoly_decrypt_ietf(key, nonce, ciphertext, aad).map_err(|e| fmt_error(&e))
 }
 
 #[wasm_bindgen]
@@ -27,12 +24,12 @@ pub fn pass_encrypt(
     salt: &[u8],
     file_format: u8,
 ) -> Result<Vec<u8>, JsValue> {
-    let pass_ver = if file_format == 0x20 {
+    let file_format = if file_format == 0x20 {
         kestrel_crypto::PassFileFormat::V1
     } else {
         return Err(fmt_error(&"Invalid password file format version"));
     };
-    let mut ciphertext: Vec<u8> = Vec::with_capacity(calc_capacity(plaintext.len()));
+    let mut ciphertext: Vec<u8> = Vec::with_capacity(calc_pass_capacity(plaintext.len()));
     let salt: [u8; 32] = salt
         .try_into()
         .map_err(|_| fmt_error(&"Invalid salt length. Must be 32."))?;
@@ -41,7 +38,7 @@ pub fn pass_encrypt(
         &mut ciphertext,
         password,
         salt,
-        pass_ver,
+        file_format,
     )
     .map_err(|e| fmt_error(&e))?;
     Ok(ciphertext)
@@ -53,15 +50,106 @@ pub fn pass_decrypt(
     password: &[u8],
     file_format: u8,
 ) -> Result<Vec<u8>, JsValue> {
-    let pass_ver = if file_format == 0x20 {
+    let file_format = if file_format == 0x20 {
         kestrel_crypto::PassFileFormat::V1
     } else {
         return Err(fmt_error(&"Invalid password file format version"));
     };
 
-    let mut plaintext: Vec<u8> = Vec::with_capacity(calc_capacity(ciphertext.len()));
-    kestrel_crypto::decrypt::pass_decrypt(&mut ciphertext, &mut plaintext, password, pass_ver)
+    let mut plaintext: Vec<u8> = Vec::with_capacity(calc_pass_capacity(ciphertext.len()));
+    kestrel_crypto::decrypt::pass_decrypt(&mut ciphertext, &mut plaintext, password, file_format)
         .map_err(|e| fmt_error(&e))?;
+    Ok(plaintext)
+}
+
+/// Key based encryption
+///
+/// For ephem_private and payload_key, pass empty slices (Uint8Array) which
+/// will be treated as the None option
+#[wasm_bindgen]
+pub fn key_encrypt(
+    mut plaintext: &[u8],
+    sender_private: &[u8],
+    recipient_public: &[u8],
+    ephem_private: &[u8],
+    payload_key: &[u8],
+    file_format: u8,
+) -> Result<Vec<u8>, JsValue> {
+    let file_format = if file_format == 0x10 {
+        kestrel_crypto::AsymFileFormat::V1
+    } else {
+        return Err(fmt_error(&"Invalid asymmetric file format version"));
+    };
+
+    let sender_private: kestrel_crypto::PrivateKey = sender_private.into();
+    let recipient_public: kestrel_crypto::PublicKey = recipient_public.into();
+    let ephem_private: Option<kestrel_crypto::PrivateKey> = if ephem_private.is_empty() {
+        None
+    } else {
+        Some(ephem_private.into())
+    };
+
+    let payload_key: Option<[u8; 32]> = if payload_key.is_empty() {
+        None
+    } else {
+        Some(
+            payload_key
+                .try_into()
+                .expect("Payload key must be 32 bytes"),
+        )
+    };
+
+    let mut ciphertext: Vec<u8> = Vec::with_capacity(calc_key_capacity(plaintext.len()));
+
+    kestrel_crypto::encrypt::key_encrypt(
+        &mut plaintext,
+        &mut ciphertext,
+        &sender_private,
+        &recipient_public,
+        ephem_private.as_ref(),
+        payload_key,
+        file_format,
+    )
+    .map_err(|e| fmt_error(&e))?;
+
+    Ok(ciphertext)
+}
+
+/// Key based decryption
+///
+/// The public_key argument must be 32 bytes in length. The resulting public
+/// key will be written into the buffer if decryption is successful.
+#[wasm_bindgen]
+pub fn key_decrypt(
+    mut ciphertext: &[u8],
+    recipient_private: &[u8],
+    file_format: u8,
+    public_key: &mut [u8],
+) -> Result<Vec<u8>, JsValue> {
+    let file_format = if file_format == 0x10 {
+        kestrel_crypto::AsymFileFormat::V1
+    } else {
+        return Err(fmt_error(&"Invalid asymmetric file format version"));
+    };
+
+    if public_key.len() != 32 {
+        return Err(fmt_error(&"Public key buf must be 32 bytes"));
+    }
+
+    let recipient_private: kestrel_crypto::PrivateKey = recipient_private.into();
+
+    let mut plaintext: Vec<u8> = Vec::with_capacity(calc_key_capacity(ciphertext.len()));
+    let sender_public_key = kestrel_crypto::decrypt::key_decrypt(
+        &mut ciphertext,
+        &mut plaintext,
+        &recipient_private,
+        file_format,
+    )
+    .map_err(|e| fmt_error(&e))?;
+    let sender_public_key = sender_public_key.as_bytes();
+
+    public_key[..32].copy_from_slice(&sender_public_key[..32]);
+
     Ok(plaintext)
 }
 
@@ -127,8 +215,12 @@ fn fmt_error(err: &dyn std::any::Any) -> JsValue {
     }
 }
 
-fn calc_capacity(len: usize) -> usize {
-    len + ((((len / 65536) + 1) * 32) + 128)
+fn calc_pass_capacity(len: usize) -> usize {
+    len + ((((len / 65536) + 1) * 32) + 36)
+}
+
+fn calc_key_capacity(len: usize) -> usize {
+    len + ((((len / 65536) + 1) * 32) + 132)
 }
 
 #[cfg(test)]
